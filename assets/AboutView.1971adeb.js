7c62a6290b99afd8b622587947fa2eb8684376e8
Dat Tran <tranthanhdat.it@gmail.com>
2022-03-19 22:06:51 +0700

    Updates

---
+++ assets/AboutView.1971adeb.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.76742547.js";import{o as t,c as o,a as _}from"./vendor.63486226.js";const s={},a={class:"about"},c=_("h1",null,"This is an about page",-1),n=[c];function r(i,u){return t(),o("div",a,n)}var f=e(s,[["render",r]]);export{f as default};
