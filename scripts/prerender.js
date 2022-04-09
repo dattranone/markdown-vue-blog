b8ad36d4fa105b3f6c431291c7145e890268b891
Dat Tran <tranthanhdat.it@gmail.com>
2022-04-10 06:43:07 +0700

    Add prerender

---
+++ scripts/prerender.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+
+const path = require('path')
+const SitePrerender = require('site-prerender')
+const blogList = require('../public/data/blog-list.json')
+
+
+;(async () => {
+  // TODO: configuration
+  const routes = blogList.reduce((prev, curr) => {
+    prev.push(`/blog/${curr.id}`)
+    return prev
+  }, [
+    '/',
+    '/about'
+  ])
+
+  const sp = new SitePrerender({
+    staticPath: path.join(__dirname, '../dist'),
+    outputFolder: path.join(__dirname, '../output'),
+    routes: ['/', '/about']
+  })
+
+  await sp.init()
+  await sp.start()
+  await sp.close()
+})()
